42b3d46c64d25e9b25a18150ce448e48cb0825e2
soycarlosherrera <carlos4505@gmail.com>
2025-04-06 02:52:45 -0500

    Create children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,16 +5,24 @@ export const routes: Routes = [
     path: 'dashboard',
     loadComponent: () =>
       import('./gifs/pages/dashboard-page/dashboard-page.component'),
-  },
-  {
-    path: 'trending',
-    loadComponent: () =>
-      import('./gifs/pages/trending-page/trending-page.component'),
-  },
-  {
-    path: 'search',
-    loadComponent: () =>
-      import('./gifs/pages/search-page/search-page.component'),
+
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page.component'),
+      },
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/search-page/search-page.component'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      },
+    ],
+
   },
   {
     path: '**',
