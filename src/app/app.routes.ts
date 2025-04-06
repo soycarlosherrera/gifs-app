d76f553a0a19f44220c070fd6c18c3cc902096b3
soycarlosherrera <carlos4505@gmail.com>
2025-04-06 00:48:22 -0500

    Create component dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
