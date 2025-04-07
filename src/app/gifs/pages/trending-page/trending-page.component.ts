0eb702340a16935991bda44ad810bd95a5be67ad
soycarlosherrera <carlos4505@gmail.com>
2025-04-07 02:14:32 -0500

    Add http get gifs

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../services/gifs.service';
 
 const imageUrls: string[] = [
   "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
@@ -23,4 +24,5 @@ const imageUrls: string[] = [
 })
 export default class TrendingPageComponent {
   gifs = signal(imageUrls);
+  gifService = inject(GifService);
  }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { inject, Injectable, signal } from '@angular/core';
+import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { environment } from '@environments/environment';
+import type { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
 
   private http = inject(HttpClient);
+
+  trendingGifs = signal<Gif[]>([]);
 
   constructor() {
     this.loadTrendingGifs();
@@ -18,6 +22,11 @@ export class GifService {
         api_key: environment.giphyApiKey,
         limit: 20,
       },
+    })
+    .subscribe((resp)=>{
+      const gifs = GifMapper.mapGiphyItemsToGiArray(resp.data);
+      this.trendingGifs.set(gifs);
+      console.log({gifs});
     });
    }
 }
